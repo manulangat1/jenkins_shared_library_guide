b9429ace929e1beacb84931721ab8514b9d9b902
manulangat <emmanuelthedeveloper@gmail.com>
2022-09-06 23:24:25 +0300

    chore: rename wrong title

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 def call(String IMAGE_NAME){ // same for all the definitions
     echo "I am building the image for branch $BRANCH_NAME"
-    withCredential([credentialId:"dockerhub-credent", passwordVariable:"PASS", usernameVariable:"USER"]) { 
+    withPassword([credentialId:"dockerhub-credent", passwordVariable:"PASS", usernameVariable:"USER"]) { 
         sh "docker build . -t $IMAGE_NAME"
         echo "$PASS |  docker login -u $USER --password-stdin"
         sh "docker push $IMAGE_NAME"
