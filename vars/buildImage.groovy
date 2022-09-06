231e017e9b974b7d2fc475c31cbcd7a4be5c06f4
manulangat <emmanuelthedeveloper@gmail.com>
2022-09-06 23:20:20 +0300

    chore: adding params

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,9 +1,9 @@
 #!/usr/bin/env groovy
-def call(){ // same for all the definitions
+def call(String IMAGE_NAME){ // same for all the definitions
     echo "I am building the image"
     withCredential([credentialId:"dockerhub-credent", passwordVariable:"PASS", usernameVariable:"USER"]) { 
-        docker build . -t IMAGE_NAME 
+        sh "docker build . -t $IMAGE_NAME"
         echo "$PASS |  docker login -u $USER --password-stdin"
-        docker push IMAGE_NAME
+        sh "docker push $IMAGE_NAME"
     }
 }
