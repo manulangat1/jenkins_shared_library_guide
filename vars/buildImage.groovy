ff58a0535d2ecb285febfb1c4f62f9eb185bfa88
manulangat <emmanuelthedeveloper@gmail.com>
2022-09-06 23:21:17 +0300

    chore add env var

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 def call(String IMAGE_NAME){ // same for all the definitions
-    echo "I am building the image"
+    echo "I am building the image for branch $BRANCH_NAME"
     withCredential([credentialId:"dockerhub-credent", passwordVariable:"PASS", usernameVariable:"USER"]) { 
         sh "docker build . -t $IMAGE_NAME"
         echo "$PASS |  docker login -u $USER --password-stdin"
