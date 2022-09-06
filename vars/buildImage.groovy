d89ed20b70e5db3f9559988d58afdb8a30548936
manulangat <emmanuelthedeveloper@gmail.com>
2022-09-06 23:25:48 +0300

    .

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 def call(String IMAGE_NAME){ // same for all the definitions
-    echo "I am building the image for branch $BRANCH_NAME"
+    echo "I am building the image for branch $BRANCH_NAME for image $IMAGE_NAME"
     withPassword([credentialId:"dockerhub-credent", passwordVariable:"PASS", usernameVariable:"USER"]) { 
         sh "docker build . -t $IMAGE_NAME"
         echo "$PASS |  docker login -u $USER --password-stdin"
